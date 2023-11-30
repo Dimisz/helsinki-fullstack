5f5b7335d4db1643cfd94f803330f5c9e4292a45
Vladimir Solovyov <leilyngale@icloud.com>
2023-11-30 15:10:53 +0800

    1.11*: unicafe step6

--- Part01IntroToReact/part1/unicafe/src/components/StatisticLine.jsx
+++ Part01IntroToReact/part1/unicafe/src/components/StatisticLine.jsx
@@ -1,7 +1,14 @@
 const StatisticLine = ({text, value}) => {
   return(
-    <p>{text} {value}</p>
-  )
+    <tr>
+      <td>
+        {text}
+      </td>
+      <td>
+        {value}
+      </td>
+    </tr>
+  );
 }
 
 export default StatisticLine;
--- Part01IntroToReact/part1/unicafe/src/components/Statistics.jsx
+++ Part01IntroToReact/part1/unicafe/src/components/Statistics.jsx
@@ -7,14 +7,16 @@ const Statistics = ({good, neutral, bad}) => {
   const average = 
     totalReviews ? (good - bad) / totalReviews : good - bad;
   return(
-    <>
-      <StatisticLine text='good' value={good}/>
-      <StatisticLine text='neutral' value={neutral}/>
-      <StatisticLine text='bad' value={bad}/>
-      <StatisticLine text='all' value={totalReviews}/>
-      <StatisticLine text='average' value={average}/>
-      <StatisticLine text='positive' value={`${positivePercentage}%`}/>
-    </>
+    <table>
+      <tbody>
+        <StatisticLine text='good' value={good}/>
+        <StatisticLine text='neutral' value={neutral}/>
+        <StatisticLine text='bad' value={bad}/>
+        <StatisticLine text='all' value={totalReviews}/>
+        <StatisticLine text='average' value={average}/>
+        <StatisticLine text='positive' value={`${positivePercentage}%`}/>
+      </tbody>
+    </table>
   );
 }
 
